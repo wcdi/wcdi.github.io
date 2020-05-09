efd73d13c00d46eced693108bbefac06945fc08f
rin1208 <sora1208sky@gmail.com>
2020-05-10 00:41:52 +0900

    add nuxt

---
+++ wcdi-web/components/Announce.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- ここにHTML書いてね -->
+  <div class="hoge">
+    <p>hoge</p>
+  </div>
+</template>
+
+<script>
+// 現状ここは無視でいいよ
+export default {};
+</script>
+
+
+
+
+<style>
+/* ここにCSS書いてね */
+
+.hoge {
+  width: 100%;
+}
+</style>
