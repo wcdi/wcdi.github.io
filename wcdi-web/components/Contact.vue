36658835f1a5d9e944972665d6e7a805ba639ae1
Explosive6363 <xjsugix@gmail.com>
2020-05-10 23:21:16 +0900

    wip: making Contact component

--- wcdi-web/components/Contact.vue
+++ wcdi-web/components/Contact.vue
@@ -1,7 +1,7 @@
 <template>
-  <!-- ここにHTML書いてね -->
-  <div class="hoge">
-    <p>hoge</p>
+  <div class=contact-section>
+      <p>コンタクト</p>
+      <button onclick="location.href='https://docs.google.com/forms/d/15WcQIkXtw05Mp33BrtDshMIHhfjJwTIquOIZpzQ69ug/edit'">Googleフォームへ</button>
   </div>
 </template>
 
@@ -10,13 +10,38 @@
 export default {};
 </script>
 
-
-
-
 <style>
 /* ここにCSS書いてね */
-
-.hoge {
+.contact-section {
+  position:relative;
   width: 100%;
+  height: 412px;
+  padding: 82px 0 82px 0;
+  background: #ffffff;
+  text-align: center;
 }
+
+.contact-section p{
+  font-style: normal;
+  font-weight: bold;
+  font-size: 34px;
+
+  color: #55AAFF;
+}
+
+.contact-section button{
+  position:relative;
+  width: 576px;
+  height: 128px;
+  bottom: -70px;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 30px;
+
+  color: #ffffff;
+  background: #555AAA;
+  border-radius: 30px;
+}
+
 </style>
