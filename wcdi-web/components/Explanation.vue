b968524a1404381c86851ef1a65e60f48699b114
mokeeemokeee <moke.0922.moke@gmail.com>
2020-05-20 02:47:58 +0900

    [change] css

--- wcdi-web/components/Explanation.vue
+++ wcdi-web/components/Explanation.vue
@@ -1,26 +1,22 @@
 <template>
   <!-- ここにHTML書いてね -->
-  <div class="Container">
-    <div class = "header">
-      <h1>情報技術研究部？</h1>
-      <br>
-      <h1>ITRC? WCDI?</h1>
+  <div class="container">
+    <div class="header">
+      <p>情報技術研究部？</p>
+      <p>ITRC? WCDI?</p>
     </div>
 
     <div class="contents">
-      <div class="main">
-        <p>
-          情報技術研究部は日本工学院八王子専門学校の部活動です<br>
-          [WCDIの説明]<br>
-          [ITRCの説明]<br>
-          [現在活動していること]
-        </p>
+      <div>
+        <p>情報技術研究部は日本工学院八王子専門学校の部活動です</p>
+        <p>[WCDIの説明]</p>
+        <p>[ITRCの説明]</p>
+        <p>[現在活動していること]</p>
       </div>
       <div class="side">
-        <img src="./../assets/サンプル画像.jpeg" alt="ITRCの関係図" title="ITRC関係図" >
+        <img src="../static/exampleImage.jpeg" alt="ITRCの関係図" title="ITRC関係図" />
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -32,34 +28,29 @@ export default {};
 
 <style scoped>
 /* ここにCSS書いてね */
-.Container {
-  background-color : rgb(236, 235, 235);
-  padding: 3rem;
-  max-width: 1920px;
-  max-height: 1080px;
-  min-width: 850px;
-  margin-right: auto;
-  margin-left: auto;
+.container {
+  background-color: rgb(236, 235, 235);
+  padding: 2rem;
   text-align: center;
 }
-.main {
-  padding: 3rem 1rem 0 0;
-}
-.side{
+.side {
   align-content: flex-end;
   height: auto;
 }
-.contents  {
-  padding: 2rem 3rem;
+.contents {
+  margin: 2rem 0;
   display: flex;
   justify-content: space-around;
 }
 .side > img {
-  width:100%;
+  width: 100%;
   max-width: 100%;
   height: auto;
 }
-.header > h1{
+.header > p {
   font-size: 5rem;
+}
+.header > p:nth-child(1) {
+  margin-bottom: 1rem;
 }
 </style>
