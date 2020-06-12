4ab37630557b3de911e2e153407c661e9f770d3e
saba383810 <saba383810@gmail.com>
2020-06-12 17:36:13 +0900

    headerコンポーネントを作成。

--- wcdi-web/components/Header.vue
+++ wcdi-web/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <p>{{ pageTitle }}</p>
-  </div>
+  <section class="top-image top-section">
+    <div class="box">
+    
+      <p>{{ pageTitle }}</p>
+      
+    </div>
+</section>
 </template>
 
 <script>
@@ -12,5 +16,28 @@
 </script>
 
 <style scoped>
+.top-image {
+  background-image: url(../static/wcdi-top1.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center top;
+  background-color: #2b2b2b;
+  background-blend-mode: soft-light;
+}
+.top-section {
+  display: flex;
+  justify-content: space-around;
+}
+.box{
+  padding: 12rem;
+}
+
+.box p {
+  top: 25%;
+  left: 40%;
+  font-size: 72px;
+  font-weight: bold;
+  color: #ffffff;
+}
 
 </style>
