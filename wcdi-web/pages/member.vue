b9ed2e1ab33b1d88dbf4ade01f80f8fbef5c3d5d
Gamou-T <47162587+Gamou-T@users.noreply.github.com>
2020-06-27 14:41:23 +0900

    modify: otherpage content

--- wcdi-web/pages/member.vue
+++ wcdi-web/pages/member.vue
@@ -3,6 +3,16 @@
     <Header pageTitle="メンバー"></Header>
     <div class="Container">
       <h2 class="title">メンバー一覧</h2>
+      <div class="content">
+        <p>情報技術研究部には日本工学院八王子専門学校の１年生から４年生が在籍しています。</p>
+        <p>情報技術を好きな学生が集まっており、日々自己研鑽に努めています。</p>
+
+        <p>メンバーにはITスペシャリスト科の学生が多いですが、日本工学院八王子専門学校の学生であればどなたでも参加することができます。</p>
+
+        <p>レベルとしては入学前から勉強していた人も、入学してからプログラミングを始めた人もいます。</p>
+        <p>サークル活動が終わったあとラーメンを食べに行ったり、休日に出かけることもあり部員同士仲が良いです。</p>
+        <p>興味がある方はぜひサークル棟３階304号室の情報技術研究部部室へ！ </p>
+      </div>
       <p class="backtotop">Top→</p>
     </div>
   </div>
@@ -35,5 +45,11 @@ p {
 .backtotop {
   font-weight: bold;
   margin-left: auto;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  margin-top: 5vh;
+  justify-content: center;
 }
 </style>
---
+++ wcdi-web/pages/news/comicmarket98.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <Header pageTitle="コミックマーケット98にサークル参加します"></Header>
+    <div class="Container">
+      <h2 class="title">概要</h2>
+      <div class="InformationContainer">
+        <ul>
+          <li><h3>場所</h3></li>
+          <p>東京ビッグサイト　南4</p>
+          <li><h3>説明</h3></li>
+          <p>東京ビックサイトやコミックマーケットについてはコミックマーケット公式サイトをご覧ください。</p>
+          <li><h3>hogehoge</h3></li>
+          <p>fugafuga</p>
+        </ul>
+        <img src="/bigsite.jpg">
+      </div>
+      <h2 class="title">最新のお知らせ一覧</h2>
+      <div class="InformationContainer">
+        <ul>
+          <li>コミックマーケット98にサークル参加します</li>
+          <li>コミックマーケット98にサークル参加します</li>
+          <li>コミックマーケット98にサークル参加します</li>
+        </ul>
+      </div>
+      <p class="backtotop">Top→</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from '@/components/Header';
+  export default {
+    name: "information",
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style scoped>
+.Container {
+  padding: 3vh 15vw;
+  display: flex;
+  flex-direction: column;
+}
+.title {
+  border-bottom: solid 3px #006F87;
+  display: inline-block;
+  margin: 0 auto 0;
+}
+p {
+  padding: 1.5vh 0;
+}
+.backtotop {
+  font-weight: bold;
+  margin-left: auto;
+}
+div.InformationContainer {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding-top: 5vh;
+}
+img {
+  max-width: 50%;
+  max-height: 50%;
+}
+</style>
