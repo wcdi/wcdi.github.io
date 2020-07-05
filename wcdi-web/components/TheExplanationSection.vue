2018eb9610abb37750eccc346b0610e6a9370a82
Taisuke1221 <n4n0k4f0rit@gmail.com>
2020-07-05 14:25:34 +0900

    Fix: Responsive layout collapse #5

--- wcdi-web/components/TheExplanationSection.vue
+++ wcdi-web/components/TheExplanationSection.vue
@@ -10,7 +10,7 @@
           <p class="text">情報技術研究部は日本工学院八王子専門学校にて<br>活動中のサークルです</p>
           <p class="text">それぞれ<strong>高い目標</strong>を持つ部員が集まり活動しています</p>
           <p class="text">定期的にもくもく会, LT大会, コンテストの参加を行い<br>日々研鑽を重ねています</p>
-         
+
           <div class="AboutUsButtonSize">
             <a id="ButtonText" href="./about">
               <div class="AboutUsButton">about us</div>
@@ -45,12 +45,9 @@
 
   .AboutUsButtonSize {
   display: flex;
-  justify-content: center;
   font-size: 1.6em;
-  padding: 0 2em;
   margin-top: 4rem;
-  margin-left: 65%;
-  margin-right: 25%;
+  margin-left: 70%;
 }
 .AboutUsButton {
   background: #208cc2;
@@ -102,6 +99,11 @@
   @media screen and (max-width: 784px) {
     .text  br {
       display: inline-block;
+    }
+    .AboutUsButtonSize {
+      margin-left: auto;
+      margin-right: auto;
+      justify-content: center;
     }
   }
 </style>
